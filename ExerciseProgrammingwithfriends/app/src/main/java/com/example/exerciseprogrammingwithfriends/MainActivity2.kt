f155a715d784d45585dccbf345125a210c9668e6
배홍식 <hongsik980305@gmail.com>
2020-07-09 13:18:03 +0900

    EXERCISE 1.(give & take)

---
+++ ExerciseProgrammingwithfriends/app/src/main/java/com/example/exerciseprogrammingwithfriends/MainActivity2.kt
@@ -0,0 +1,18 @@
+package com.example.exerciseprogrammingwithfriends
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("zerom",10)
+        startActivity(intent)
+
+    }
+
+}
